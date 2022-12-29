91475db586f19e87e82cac6d2f235c43580ed3fa
Nascccom <ostrov_o@bk.ru>
2022-12-29 17:39:16 +0300

    Change import in index.tsx (rename reset.css)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/reset.css';
+import './reset-css/reset.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
